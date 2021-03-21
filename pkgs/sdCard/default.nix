0d075580d2ade8c6fa7c616f649af9c1b0a7b6b0
Ana Hobden <operator@hoverbear.org>
2021-03-21 08:10:56 -0700

    --amend
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- pkgs/sdCard/default.nix
+++ pkgs/sdCard/default.nix
@@ -3,6 +3,6 @@
 runCommandNoCC "lx2k-sdcard.img" { } ''
   set -x
   truncate -s 2160066560 $out
+  dd of=$out if=${isoImage.isoImage}/iso/*.iso
   dd of=$out if=${uefi}
-  dd iflag=skip_bytes of=$out if=${isoImage.isoImage}/iso/*.iso skip=8388608
 ''
